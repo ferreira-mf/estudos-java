75f32a6aa5b71544918aee72e2a4b015ebb6e326
ferreira-mf <marcelo.ferreira9@live.com>
2024-07-02 20:48:30 -0300

    iniciando estudos em java

---
+++ src/Main.java
@@ -0,0 +1,101 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+//    //DECLARACAO DE VARIAVEIS
+//        int y = 32;
+//        double x = 10.35784;
+//
+//        String nome = "Maria";
+//        int idade = 31;
+//        double renda = 4000.0;
+//
+//
+//
+//    //ESCREVER NA TELA
+//        System.out.print("Bom dia!"); // print não quebra a linha
+//        System.out.println("Olá Mundo!"); // println quebra a linha
+//        System.out.println(y); //println contendo variavel
+//        System.out.println(x); //println contendo variavel
+//        System.out.printf("%.2f%n", x); //prinf conta casa decimal exibida, 2f% significa a quantidade de casa e o %n é a quebra de linha
+//        System.out.println("Resultado = " + x + " Metros"); // Concatenar/Concatenação
+//        System.out.printf("Resultado = %.2f metros%n", x); // Concatenação onde a variavel entra no %.2f (quantidade de quebra de linha)
+//        System.out.printf("%s tem %d anos e ganha R$ %.2f reais.%n", nome, idade, renda); //Concatenação de varios elementos onde: %f = ponto flutuante, %d = inteiro, %s = texto, %n = quebra de linha
+//
+//    //PROCESSAMENTO DE DADOS
+//
+//        //Variavel onde há operaçoes
+//        int q = 5;
+//        int w = 2 * q;
+//
+//        System.out.println("variavel de q é igual a: " + q);
+//        System.out.println("variavel de w é igual a: " + w);
+//
+//        //Calcula da área de um trapézio
+//        double b, B, h, areaTrapezio;
+//        b = 6.0;
+//        B = 8.0;
+//        h = 5.0;
+//        areaTrapezio = (b + B) / 2 * h;
+//        System.out.println("a area do trapézio é igual a: " + areaTrapezio);
+//
+//        //DIVISAO DE NUMEROS INTEIROS, QUANDO O VALOR DA NUMERO QUEBRADO
+//        int o, p;
+//        double resultado;
+//
+//        o = 5;
+//        p = 2;
+//        resultado = (double) o / p;
+//        System.out.println("5 dividido por 2 é igual a: " + resultado);
+//
+//
+//        //INPUT DE DADOS (necessário importar: import java.util.Scanner;)
+        Scanner sc = new Scanner (System.in); //criaçao da variavel que associa o Scanner ao teclado no modo console
+//
+//        //Input de texto: next() lê a String até o primeiro espaço enquanto nextLine() lê até encontrar uma quebra de linha.
+//        String digitarString = sc.next();
+//        System.out.println("1 - Você digitou: " + digitarString);
+//
+//        //Input para digitar inteiros
+//        int digitarInt = sc.nextInt();
+//        System.out.println("2 - Você digitou: " + digitarInt);
+//
+//        //Input para digitar double
+//        double digitarDouble = sc.nextDouble();
+//        System.out.println("3 - Você digitou: " + digitarDouble);
+//
+//        //Input para digitar um caractere
+//        char digitarChar = sc.next().charAt(0);
+//        System.out.println("3 - Você digitou: " + digitarChar);
+//
+//        //Input de mais de um dado em uma só linha (apertando 1 somente o enter)
+//        String linhaUm = sc.next();
+//        int linhaDois = sc.nextInt();
+//        double linhaTres = sc.nextDouble();
+//        System.out.println("Dados digitados");
+//        System.out.println(linhaUm);
+//        System.out.println(linhaDois);
+//        System.out.println(linhaTres);
+//        sc.close();
+
+    //Input de texto: next() lê a String até o primeiro espaço enquanto nextLine() lê até encontrar uma quebra de linha.
+    String s1, s2, s3;
+    s1 = sc.nextLine();
+    s2 = sc.nextLine();
+    s3 = sc.nextLine();
+
+    System.out.println("DADOS DIGITADOS:");
+    //sc.nextLine(); <--- EXEMPLO: LEIA A OBS ABAIXO
+    System.out.println(s1);
+    System.out.println(s2);
+    System.out.println(s3);
+    }
+
+    /*OBS: Quando você usa um comando de leitura diferente de nextLine(), (nextInt() por exemplo) e dá alguma quebra de linha, essa quebra  de
+    linha fica pendente na entrada padrão... SOLUÇÂO: Faça um nextLine() extra antes de fazer o nextLine() de seu interesse)
+     */
+
+
+
+}
