244dd16e547246297bfe3c451f7ddc3a677116a7
ferreira-mf <marcelo.ferreira9@live.com>
2024-08-08 00:52:51 -0300

    realizacao ex03 poo

---
+++ src/conteudo/orientacaoobjetos/Exercicioestudante.java
@@ -0,0 +1,32 @@
+package conteudo.orientacaoobjetos;
+
+import conteudo.orientacaoobjetos.entities.Student;
+
+import java.util.Scanner;
+
+public class Exercicioestudante {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Student student = new Student();
+
+        System.out.println("Insira o nome do aluno: ");
+        student.name = sc.nextLine();
+        System.out.println("Insira a primeira nota: ");
+        student.n1 = sc.nextDouble();
+        System.out.println("Insira a segunda nota: ");
+        student.n2 = sc.nextDouble();
+        System.out.println("Insira a terceira nota: ");
+        student.n3 = sc.nextDouble();
+
+        System.out.println(student.name + " = " + student.notaFinal());
+
+        if (student.notaFinal() >= 60.00) {
+            System.out.println("Aprovado");
+        } else {
+            System.out.printf("Reprovado, falta %.2f pontos%n", student.aprovacao());
+        }
+        sc.close();
+    }
+}
---
+++ src/conteudo/orientacaoobjetos/entities/Student.java
@@ -0,0 +1,22 @@
+package conteudo.orientacaoobjetos.entities;
+
+public class Student {
+
+    public String name;
+    public double n1, n2, n3;
+
+
+    public double notaFinal() {
+        return this.n1 + this.n2 + this.n3;
+    }
+
+    public double aprovacao() {
+        if (notaFinal() < 60.00) {
+            return 60.0 - notaFinal();
+        }
+            else {
+                return 0.0;
+        }
+
+    }
+}
