9495c38bbfecbc7b6107c4cdac9c163a710289b2
Marcelo Ferreira <marcelo.ferreira9@live.com>
2024-09-24 23:18:58 -0300

    get and setters

---
+++ src/conteudo/orientacaoobjetos/GetAndSetters.java
@@ -0,0 +1,25 @@
+package conteudo.orientacaoobjetos;
+
+public class GetAndSetters {
+
+    //ENCAPSULAMENTO / Get e Setters : LINK DA AULA: https://www.udemy.com/course/java-curso-completo/learn/lecture/10566042#overview
+
+
+    //PADRÃO PARA IMPLEMENTAÇÃO DE GETTERS E SETTERS
+
+    //vamos supor que temos a classe "Produto" e a classe tem os atributos name e price:
+    private String name; //como esses atributos tem a palavra private, eles não podem ser acessados por outras classes.
+    private double price;
+
+    //caso eu precise ACESSAR um atributo:
+    public String getName() {   //Aqui eu estou acessando o atributo "name", onde eu preciso escrever um metodo get + Nome do atributo (getName) (obs: o nome do atributo precisa começar com letra maisucula, ex: getPrice etc...)
+        return name;
+    } //OBS: É preciso colocar o type no metodo igual ao do atributo, se o atributo for String, o metodo tambem precisa ser.
+
+    //caso eu precise  ALTERAR um atributo:
+    public void setName (String name) {  //como esse metodo não retorna nada, ele será void. Como ele sera alterado, ele precisa RECEBER um novo valor pro atributo, que é o parametro a ser inserido...
+        this.name = name; //aqui ele mostra que this.name (atributo) irá receber o valor de parametro String name (novo valor que sera inserido)
+    }
+}
+
+
--- src/conteudo/orientacaoobjetos/entities/Product.java
+++ src/conteudo/orientacaoobjetos/entities/Product.java
@@ -13,20 +13,28 @@ public class Product { //classe
         this.quantity = quantity;
     }
 
+    //Metodo para retornar valor total em estoque
     public double totalValueInStock() { //metodo da classe Product (valor total em estoque)
         return price * quantity;
     }
 
+    //Metodo para adicionar produtos em estoque
     public void addProducts(int quantity) { //outro metodo da classe Product
         this.quantity += quantity; //A palavra-chave this é usada para se referir ao atributo quantity da própria instância da classe, diferenciando-o do parâmetro quantity do método.
 
     }
 
+    //Metodo para remover produtos em estoque
     public void removeProducts(int quantity) {
         this.quantity -= quantity;
     }
 
+    //Metodo para converter o objeto para string
     public String toString() {
-        return name + ", $ " + String.format("%.2f", price) + ", " + quantity + " units, Total: $ " + String.format("%.2f", totalValueInStock());
+        return name + ", $ "
+                + String.format("%.2f", price)
+                + ", " + quantity
+                + " units, Total: $ "
+                + String.format("%.2f", totalValueInStock());
     }
 }
