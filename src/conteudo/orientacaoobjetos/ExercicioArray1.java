67a00302b0694b77b2373d5a65764cf18f2cbb16
Marcelo Ferreira <marcelo.ferreira9@live.com>
2024-10-17 22:59:28 -0300

    exercicios arrays

---
+++ src/conteudo/orientacaoobjetos/ExercicioArray1.java
@@ -0,0 +1,33 @@
+package conteudo.orientacaoobjetos;
+
+import java.util.Scanner;
+
+public class ExercicioArray1 {
+    public static void main(String[] args) {
+
+        //Digitar a variavel N, digitar N numeros e printar os numeros negativos
+
+        //link exercicio: https://www.udemy.com/course/java-curso-completo/learn/lecture/32875592#questions
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantos numeros voce vai digitar? ");
+        int N = sc.nextInt();
+        double vetor [] = new double[N];
+
+
+        for (int i=0; i< vetor.length; i++ ) {
+            System.out.println("Digite um numero: ");
+            vetor [i] = sc.nextInt();
+        }
+
+        System.out.println("numeros negativos: ");
+        for (int i=0; i< vetor.length; i++) {
+            if (vetor[i]<0) {
+                System.out.println(vetor[i]);
+            }
+        }
+
+        sc.close();
+    }
+}
---
+++ src/conteudo/orientacaoobjetos/entities/ProductArray2.java
@@ -0,0 +1,29 @@
+package conteudo.orientacaoobjetos.entities;
+
+public class ProductArray2 {
+
+    private String name;
+    private double price;
+
+
+    public ProductArray2(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
